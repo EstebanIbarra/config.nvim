ac989b219686044b0acd77a44020d62505848d62
Esteban Ibarra <sectorjeat@gmail.com>
2023-10-11 15:10:12 -0600

    Bumps telescope version and fixes some remaps

--- lua/eir/packer.lua
+++ lua/eir/packer.lua
@@ -1,10 +1,11 @@
+---@diagnostic disable: undefined-global
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
@@ -29,8 +30,6 @@ return require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter-context'
 
   use 'theprimeagen/harpoon'
-
-  -- use 'theprimeagen/refactoring.nvim'
 
   use 'mbbill/undotree'
 
