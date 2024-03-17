7e45d3b94d054e65c1a9fe1e11c778b3c1a3da29
Esteban Ibarra <sectorjeat@gmail.com>
2024-03-17 11:43:12 -0600

    Updates supercollider config

--- after/plugin/supercollider.lua
+++ after/plugin/supercollider.lua
@@ -2,17 +2,46 @@ local scnvim = require('scnvim')
 local map = scnvim.map
 local map_expr = scnvim.map_expr
 
+-- Find pandoc
+local handle = io.popen('which pandoc')
+local pandoc = handle:read('*a')
+handle:close()
+pandoc = pandoc:gsub('^%s*(.-)%s*$', '%1')
+assert(pandoc ~= '', 'Pandoc not found')
+
 scnvim.setup({
   keymaps = {
-    ['<leader>scl'] = map('editor.send_line', {'i', 'n'}),
-    ['<leader>scb'] = {
-      map('editor.send_block', {'i', 'n'}),
-      map('editor.send_selection', 'x')
-    },
+    -- Global Mappings
     ['<leader>scs'] = map('sclang.start'),
-    ['<leader>sck'] = map('sclang.hard_stop', {'n', 'x', 'i'}),
     ['<leader>scr'] = map('sclang.recompile'),
-    ['<leader>scb'] = map_expr('s.boot'),
-    ['<leader>scm'] = map_expr('s.meter'),
+    -- Playback Mappings
+    ['<leader>scl'] = map('editor.send_line', 'n'),
+    ['<leader>sce'] = {
+      map('editor.send_block', 'n'),
+      map('editor.send_selection', 'v'),
+    },
+    ['<leader>sck'] = map('sclang.hard_stop', {'n', 'v'}),
+    -- Server Mappings
+    ['<leader>scsb'] = map_expr('s.boot'),
+    ['<leader>scsr'] = map_expr('s.reboot'),
+    ['<leader>scsk'] = map_expr('s.quit'),
+    ['<leader>scsm'] = map_expr('s.meter'),
+    -- Post Window Mappings
+    ['<leader>scwt'] = map('postwin.toggle', {'n', 'v'}),
+    ['<leader>scwc'] = map('postwin.open', {'n', 'v'}),
+    ['<leader>scwx'] = map('postwin.close', {'n', 'v'}),
+    ['<leader>scwd'] = map('postwin.clear', {'n', 'v'}),
+  },
+  postwin = {
+    highlight = true,
+    horizontal = true,
+    direction = 'bot',
+    size = 7,
+    fixed_size = 7,
+  },
+  documentation = {
+    cmd = pandoc,
+    direction = 'right',
+    horizontal = false,
   },
 })
